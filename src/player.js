99e2143e59ed33e40af16c18495aa7f8b9491ab7
Diego Comfort <812559@seq.org>
2024-02-01 22:42:00 -0800

    Created, and linked, Player
    This could be a super class for enemy, hero

--- src/player.js
+++ src/player.js
@@ -1,5 +1,28 @@
-class Player() {
-    constructor() {
-        
+/**
+ * Player - a super class for characters, enemies, and who knows what else
+ */
+class Player {
+    /**
+     * Player constructor
+     * @param {World} world - the world the player belongs to
+     * @param {JSVector} position - the character's position in the maze
+     * @param {number} width - the character's width (1 is the size of a maze cell)
+     * @param {number} [maxHealth=100] - the character's max health
+     */
+    constructor(world, position, width, maxHealth=100) {
+        /* @type {World} */
+        this.world = world;
+
+        /* @type {JSVector} */
+        this.position = position;
+
+        /* @type {number} */
+        this.width = width;
+
+        /* @type {number} */
+        this.maxHealth = maxHealth;
+
+        /* @type {number} */
+        this.health = maxHealth;
     }
 }
