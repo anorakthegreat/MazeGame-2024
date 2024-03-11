ccde14227669d94907907683c90bdc0ed07aa173
Calvin <146762695+812967@users.noreply.github.com>
2024-03-11 10:35:14 -0700

    Justin's pull request merged in

--- src/world.js
+++ src/world.js
@@ -33,18 +33,18 @@ class World {
 
     run() {
         this.framecount++
-      
+
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
         this.maze.render(this.renderCenter);
-        // for (const enemy of this.enemies) {
-        //     enemy.run(this.renderCenter);
-        // }
+        for (const enemy of this.enemies) {
+            enemy.run(this.renderCenter);
+        }
 
         this.hero.run(this.context, this.canvas, this.maze);
         this.updateStatusBar();
     }
-    updateStatusBar(){
+    updateStatusBar() {
         this.updateTimer();
         this.runScore();
     }
