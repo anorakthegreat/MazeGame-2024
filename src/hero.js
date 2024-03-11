8c528c1f57022bd29ea49e477061f9f095eff985
Justin Lee <810636@seq.org>
2024-03-11 10:25:42 -0700

    fixed sdog deleting my oxygen code

--- src/hero.js
+++ src/hero.js
@@ -2,43 +2,25 @@ function Hero(m) {
 
     this.mazePosition = new JSVector(1, 1)
     this.loc = new JSVector(1, 1)
-    this.maze = m
-    this.moveIncrement = 3
+    this.maze = m;
+    this.moveIncrement = 3;
+    this.health = 100;
+    this.oxygen = 100;
 
     window.addEventListener("keydown", (event) => {
-        // switch (event.key) {
-        // case "ArrowDown":
-        //     // this.loc.y+=this.moveIncrement;
-        //     this.moveDown()
-        //     break;
-        // case "ArrowUp":
-        //     // this.loc.y-=this.moveIncrement;
-        //     this.moveUp()
-        //     break;
-        // case "ArrowLeft":
-        //     this.moveLeft()
-        //     break;
-        // case "ArrowRight":
-        //     this.moveRight();
-        //     break
-        // default:
-        //     break;
-        // }
-
-
-        if(event.key == "ArrowDown"){
+        if (event.key == "s") {
             this.moveDown()
         }
 
-        if(event.key == "ArrowUp"){
+        if (event.key == "w") {
             this.moveUp()
         }
 
-        if(event.key == "ArrowLeft"){
+        if (event.key == "a") {
             this.moveLeft()
         }
 
-        if(event.key == "ArrowRight"){
+        if (event.key == "d") {
             this.moveRight()
         }
     });
@@ -46,37 +28,31 @@ function Hero(m) {
 
 }
 
-Hero.prototype.moveUp = function(){
-    // console.log(this.getMazeUp(sworld.maze))
-    if(this.getMazeUp(world.maze) == false){
-        this.loc.y -= this.moveIncrement
+Hero.prototype.moveUp = function () {
+    if (this.getMazeUp(world.maze) == false) {
+        this.loc.y -= this.moveIncrement;
     }
 }
 
-Hero.prototype.moveDown = function(){
-    // console.log(this.getMazeDown(world.maze))
-    if(this.getMazeDown(world.maze) == false){
-        this.loc.y += this.moveIncrement
+Hero.prototype.moveDown = function () {
+    if (this.getMazeDown(world.maze) == false) {
+        this.loc.y += this.moveIncrement;
     }
 }
 
-Hero.prototype.moveLeft = function(){
-    // console.log(this.getMazeLeft(world.maze))
-    if(this.getMazeLeft(world.maze) == false){
-        this.loc.x -= this.moveIncrement
+Hero.prototype.moveLeft = function () {
+    if (this.getMazeLeft(world.maze) == false) {
+        this.loc.x -= this.moveIncrement;
     }
-    // console.log("BALLS")
 }
 
-Hero.prototype.moveRight = function(){
-    // console.log(this.getMazeLeft(world.maze))
-    if(this.getMazeRight(world.maze) == false){
-        this.loc.x += this.moveIncrement
+Hero.prototype.moveRight = function () {
+    if (this.getMazeRight(world.maze) == false) {
+        this.loc.x += this.moveIncrement;
     }
-    // console.log("BALLS")
 }
 
-Hero.prototype.areInContact = function(square, rectangle) {
+Hero.prototype.areInContact = function (square, rectangle) {
     // Extracting coordinates and dimensions for easier comparison
     const squareX = square.x;
     const squareY = square.y;
@@ -95,140 +71,114 @@ Hero.prototype.areInContact = function(square, rectangle) {
     return isOverlapX && isOverlapY;
 }
 
-Hero.prototype.run = function(ctx, canvas, maze){
+Hero.prototype.run = function (ctx, canvas, maze) {
     // this.updateCanvas(ctx)
 
     // this.updateCanvas(ctx, canvas)
-    this.render(ctx, canvas)
+    this.render(ctx, canvas);
 
-    this.returnMazeLoc(maze)
+    this.returnMazeLoc(maze);
+    this.updateStatusBar();
 }
 
-Hero.prototype.getMazeUp = function(maze){
-    let x = Math.ceil((this.loc.x)/ 50)
-    let y = Math.ceil((this.loc.y)/50)
+Hero.prototype.getMazeUp = function (maze) {
+    let x = Math.ceil((this.loc.x) / 50);
+    let y = Math.ceil((this.loc.y) / 50);
 
-    let cell = maze.getCell(y - 1, x - 1)
+    let cell = maze.getCell(y - 1, x - 1);
 
-    x = x-1
-    y = y-1
+    x = x - 1;
+    y = y - 1;
 
-    // console.log("X: " + x)
-    // console.log("Y: " + y)
-    // console.log("RAHHHH")
-    // console.log("HAS TOP WALL " +s cell.topWall())
 
-    if(cell.topWall()){
-        let position = {x: this.loc.x, y: this.loc.y, size: 5}
-        let wall = {x: cell.topLy, y: cell.topLx, width: cell.cellWidth, height: cell.wallWidth}
-        // console.log("ABS  " + Math.abs(position.y - wall.x))
-        // console.log("WALLLL Y " + wall.x)
-        if(Math.abs(position.y - wall.x) < 8){
-            return true
-        } else{
-            return false
+    if (cell.topWall()) {
+        let position = { x: this.loc.x, y: this.loc.y, size: 5 }
+        let wall = { x: cell.topLy, y: cell.topLx, width: cell.cellWidth, height: cell.wallWidth }
+        if (Math.abs(position.y - wall.x) < 8) {
+            return true;
+        } else {
+            return false;
         }
-       
-    } else{
-        return false
+
+    } else {
+        return false;
     }
 
 
 }
 
-Hero.prototype.getMazeDown = function(maze){
-    let x = Math.ceil((this.loc.x)/ 50)
-    let y = Math.ceil((this.loc.y)/50)
+Hero.prototype.getMazeDown = function (maze) {
+    let x = Math.ceil((this.loc.x) / 50);
+    let y = Math.ceil((this.loc.y) / 50);
 
-    let cell = maze.getCell(y - 1, x - 1)
+    let cell = maze.getCell(y - 1, x - 1);
 
-    x = x-1
-    y = y-1
+    x = x - 1;
+    y = y - 1;
 
-    // console.log("X: " + x)
-    // console.log("Y: " + y)
-    // console.log("RAHHHH")
-    // console.log("HAS TOP WALL " + cell.topWall())
-
-    if(cell.bottomWall()){
-        let position = {x: this.loc.x, y: this.loc.y, size: 5}
-        let wall = {x: cell.bottomLy, y: cell.bottomLx, width: cell.cellWidth, height: cell.wallWidth}
-
-        // console.log("ABS  " + Math.abs(position.y - wall.x))
-        // console.log("WALLLL Y " + wall.x)
-        if(Math.abs(position.y - wall.x) < 8){
-            return true
-        } else{
-            return false
+    if (cell.bottomWall()) {
+        let position = { x: this.loc.x, y: this.loc.y, size: 5 }
+        let wall = { x: cell.bottomLy, y: cell.bottomLx, width: cell.cellWidth, height: cell.wallWidth }
+        if (Math.abs(position.y - wall.x) < 8) {
+            return true;
+        } else {
+            return false;
         }
-       
-    } else{
-        return false
+
+    } else {
+        return false;
     }
 
 
 }
 
-Hero.prototype.getMazeLeft = function(maze){
-    let x = Math.ceil((this.loc.x)/ 50)
-    let y = Math.ceil((this.loc.y)/50)
+Hero.prototype.getMazeLeft = function (maze) {
+    let x = Math.ceil((this.loc.x) / 50);
+    let y = Math.ceil((this.loc.y) / 50);
 
-    let cell = maze.getCell(y - 1, x - 1)
+    let cell = maze.getCell(y - 1, x - 1);
 
-    x = x-1
-    y = y-1
+    x = x - 1;
+    y = y - 1;
 
-    // console.log("X: " + x)
-    // console.log("Y: " + y)
-    // console.log("RAHHHH")
-    console.log("HAS LEFT WALL " + cell.leftWall())
 
-    if(cell.leftWall()){
-        let position = {x: this.loc.x, y: this.loc.y, size: 5}
-        let wall = {x: cell.bottomLy, y: cell.bottomLx, width: cell.cellWidth, height: cell.wallWidth}
 
-        console.log("ABS  " + Math.abs(position.y - wall.x))
-        console.log("WALLLL Y " + wall.x)
-        if(Math.abs(position.x - wall.y) < 8){
-            return true
-        } else{
-            return false
+    if (cell.leftWall()) {
+        let position = { x: this.loc.x, y: this.loc.y, size: 5 };
+        let wall = { x: cell.bottomLy, y: cell.bottomLx, width: cell.cellWidth, height: cell.wallWidth };
+
+        if (Math.abs(position.x - wall.y) < 8) {
+            return true;
+        } else {
+            return false;
         }
-       
-    } else{
-        return false
+
+    } else {
+        return false;
     }
 
 
 }
 
-Hero.prototype.getMazeRight = function(maze){
-    let x = Math.ceil((this.loc.x)/ 50)
-    let y = Math.ceil((this.loc.y)/50)
+Hero.prototype.getMazeRight = function (maze) {
+    let x = Math.ceil((this.loc.x) / 50)
+    let y = Math.ceil((this.loc.y) / 50)
 
     let cell = maze.getCell(y - 1, x - 1)
 
-    x = x-1
-    y = y-1
+    x = x - 1
+    y = y - 1
 
-    // console.log("X: " + x)
-    // console.log("Y: " + y)
-    // console.log("RAHHHH")
-    // console.log("HAS LEFT WALL " + cell.leftWall())
-
-    if(cell.rightWall()){
-        let position = {x: this.loc.x, y: this.loc.y, size: 5}
-        let wall = {x: cell.bottomLy, y: cell.bottomRx, width: cell.cellWidth, height: cell.wallWidth}
-
-        console.log("ABS  " + Math.abs(position.y - wall.x))
-        console.log("WALLLL Y " + wall.x)
-        if(Math.abs(position.x - wall.y) < 8){
+    if (cell.rightWall()) {
+        let position = { x: this.loc.x, y: this.loc.y, size: 5 }
+        let wall = { x: cell.bottomLy, y: cell.bottomRx, width: cell.cellWidth, height: cell.wallWidth }
+        if (Math.abs(position.x - wall.y) < 8) {
             return true
-        } else{
+        } else {
             return false
         }
-       
-    } else{
+
+    } else {
         return false
     }
 
@@ -236,28 +186,62 @@ Hero.prototype.getMazeRight = function(maze){
 }
 
 
-Hero.prototype.returnMazeLoc = function(maze){
-    let x = Math.ceil((this.loc.x)/ 50)
-    let y = Math.ceil((this.loc.y)/50)
+Hero.prototype.returnMazeLoc = function (maze) {
+    let x = Math.ceil((this.loc.x) / 50)
+    let y = Math.ceil((this.loc.y) / 50)
 
     let cell = maze.getCell(x, y)
 
     this.mazePosition = new JSVector(x, y)
-    // console.log("X" + x) 
-    // console.log("Y" + y) 
 
-    
+
 
 
 }
 
 
-Hero.prototype.render = function(ctx, canvas){
+Hero.prototype.render = function (ctx, canvas) {
     // ctx.clearRect(canvas.width, canvas.height, canvas.width * 2, canvas.height * 2);
     ctx.strokeStyle = "rgba(255, 0, 0, 55)"
     ctx.fillStyle = "rgba(255, 0, 0, 55)"
     ctx.beginPath();
     ctx.arc(this.loc.x, this.loc.y, 5, Math.PI * 2, 0, false);
     ctx.stroke();
-    ctx.fill() 
+    ctx.fill()
 }
+Hero.prototype.getMazeLocation = function () {
+    let x = Math.floor((this.loc.x) / 50)
+    let y = Math.floor((this.loc.y) / 50)
+    let cell = world.maze.getCell(y, x)
+    return cell;
+}
+Hero.prototype.updateStatusBar = function () {
+    this.updateHealth();
+    this.updateOxygen();
+}
+Hero.prototype.updateHealth = function () {//assume max health will always be 100
+    let h = document.getElementById("health");
+    let hB = document.getElementsByClassName("infoTile");
+    let hP = Math.round(this.health) / 100;
+    h.innerHTML = hP * 100 + "%";
+    //color change not working rn
+    hB.item(0).style.color = "rgb(23," + 115 * hP + ",41)";
+}
+Hero.prototype.updateOxygen = function () {
+    this.oxygen -= 0.01;
+    if (this.oxygen <= 0 && this.health > 0) {
+        this.health -= 0.1;
+    } else if (this.oxygen < 10 && this.health > 0) {
+        this.health -= 0.01;
+    } else if (this.oxygen < 30 && this.health > 0) {
+        this.health -= 0.001;
+    }
+    let o = document.getElementById("oxygen");
+    let oP = 0;
+    if (this.oxygen > 0) {
+        oP = Math.round(this.oxygen) / 100;
+    }
+    o.innerHTML = oP * 100 + "%";
+    //need to add color change still
+}
+
