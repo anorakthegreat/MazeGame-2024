3a46190e7d4fc809bf6737eda99f739cdb7eaffd
diego <812559>
2024-03-12 19:06:33 -0700

    Correctly initialized hero in Maze.regenerate()

--- src/maze.js
+++ src/maze.js
@@ -47,7 +47,7 @@ Maze.prototype.regenerate = function () {
     this.explore(0, 0);
     this.entryExit();
     //reset hero 
-    this.world.hero = new Hero(this.world);
+    this.world.hero = new Hero(this.world.maze);
     //reset enemies 
     this.world.enemies = [];
     this.world.enemies[0] = new Enemy(this.world, new JSVector(10.5, 10.5));
