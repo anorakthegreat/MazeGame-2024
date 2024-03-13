c11cd2886cbb6989042126ce539f4b2e223ada05
Calvin <146762695+812967@users.noreply.github.com>
2024-03-13 09:57:28 -0700

    not quite sure

--- src/levels.js
+++ src/levels.js
@@ -2,7 +2,7 @@ class Level {
     constructor(world, num) {
         this.world = world;
         this.index = num;
-        this.renderCenter;
+        this.renderCenter = false;
         this.maze;
         this.hero;
         this.enemies = [];
@@ -13,29 +13,41 @@ class Level {
     }
 
     baseLevel() {
-
         //make maze
         this.maze = new Maze(this.world, 15, 15, this.renderCenter);
         this.maze.regenerate(true);
-        world.maze = this.maze;
+        // this.world.maze = this.maze;
         //make hero and enemies 
         this.hero = new Hero(this.maze);
-        world.hero = this.hero;
+        // this.world.hero = this.hero;
         this.enemies = [];
         for(let i = 0; i<this.index+1; i++){
-            this.enemies[i] = new Enemy(world, new JSVector(10, 10));
-            world.enemies[i] = this.enemies[i];
+            this.enemies[i] = new Enemy(this.world, new JSVector(10, 10));
         }
-
         //reset html elements 
-        this.world.paused = false;
-        this.world.time = 0;
-        this.world.msTime = 0;
-        this.world.score = 0;
+        this.paused = false;
+        this.time = 0;
+        this.msTime = 0;
+        this.score = 0;
     }
 
     nextLevel() {
+        //push a new level into the array 
         levels.push(new Level(world, levels.length));
         levels[levels.length-1].baseLevel();
+    }
+
+    setLevel(){
+        //set properties of world to a specific level 
+        world.renderCenter = this.renderCenter;
+        world.maze = this.maze;
+        world.hero = this.hero;
+        world.enemies = this.enemies;
+        world.paused = this.paused;
+        world.time = this.time;
+        world.msTime = this.msTime;
+        world.score = this.score;
+
+        world.currentLevel = this.index;
     }
 }
--- src/main.js
+++ src/main.js
@@ -8,13 +8,16 @@ function init() {
     world = new World();
     levels.push(new Level(world, 0));
     levels[0].baseLevel();
-    //levels[0].nextLevel();
+    levels[0].setLevel();
     addAllListeners();
     run();
 }
 
 function run() {
     window.requestAnimationFrame(run);
+    if(Math.random()*1000>999){
+        levels[levels.length-1].nextLevel();
+    }
     if(!world.paused){
         world.run();
     }
