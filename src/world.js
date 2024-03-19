94f2e1416c1cdfc2b0765cc5776a1b73028f056f
Calvin <146762695+812967@users.noreply.github.com>
2024-03-19 10:40:46 -0700

    Oxygen bubble gets smaller when air is used up

--- src/world.js
+++ src/world.js
@@ -26,7 +26,7 @@ class World {
         this.score = 0;
 
         this.currentLevel = 0;
-        this.levels = [new Level(15, 15, 1, true)];
+        this.levels = [new Level(15, 15, 1, false)];//rows, cols, level number, renderCenter 
         this.genLevel(this.levels[0]);
     }
 
